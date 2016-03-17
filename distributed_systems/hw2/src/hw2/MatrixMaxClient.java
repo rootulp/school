d1c9034f27c94f4a5e11c0e16bb784c344d0e299
Rootul Patel <rootulp@gmail.com>
2016-03-17 17:08:57 -0400

    hw2: threading for server
    Also added ability for client to connect to remote server
    Decided to not create multiple threads per client but could be changed

--- distributed_systems/hw2/src/hw2/MatrixMaxClient.java
+++ distributed_systems/hw2/src/hw2/MatrixMaxClient.java
@@ -8,12 +8,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MatrixMaxClient {
+	
+	private static class MatrixMaxClientThread extends Thread {
+        
+        private String matrixMaxServerAddress;
+        private int matrixMaxServerPort;
+        private Socket matrixMaxServerSocket;
+        
+        public MatrixMaxClientThread(String matrixMaxServerAddress, int matrixMaxServerPort) {
+            this.matrixMaxServerAddress = matrixMaxServerAddress;
+            this.matrixMaxServerPort = matrixMaxServerPort;
+        }
+
+        public void run() {
+            try {
+            	matrixMaxServerSocket = new Socket(matrixMaxServerAddress, matrixMaxServerPort);
+            	
+            	// Stream Setup
+        	    ObjectInputStream ois = new ObjectInputStream(matrixMaxServerSocket.getInputStream());
+        	    ObjectOutputStream oos = new ObjectOutputStream(matrixMaxServerSocket.getOutputStream());
+
+        	    // Read rowNum and matrixMax
+        	    int rowNum = ois.readInt();
+        	    MatrixMax matrixMax = (MatrixMax) ois.readObject();
+        	    
+        	    // Print rowNum and matrix
+        	    System.out.println("Row num: " + rowNum);
+        	    System.out.println();
+        	    matrixMax.print();
+        	    
+        	    int maxForRow = findMax(matrixMax.getRow(rowNum));
+        	    
+        	    // Print max
+        	    System.out.println("Max: " + maxForRow);
+        	    
+        	    oos.writeObject(Integer.toString(maxForRow));
+        	    
+        	    // Close Streams
+        	    ois.close();
+        	    oos.close();
+            } catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+                try {
+                	matrixMaxServerSocket.close();
+                } catch (IOException e) {
+                	// TODO Auto-generated catch block
+    				e.printStackTrace();
+                }
+            }
+        }
+    }
 	
 	private static int findMax(int[] row) {
 		int max = row[0];
@@ -27,36 +81,14 @@ public class MatrixMaxClient {
 	
 	public static void main(String argv[]) throws IOException, ClassNotFoundException {
 		
-		// Socket Setup
-	    Socket socket;
-	    int portNumber = 1777;
-	    socket = new Socket(InetAddress.getLocalHost(), portNumber);
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 	    
-	    // Stream Setup
-	    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-	    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-
-	    // Read rowNum and matrixMax
-	    int rowNum = ois.readInt();
-	    MatrixMax matrixMax = (MatrixMax) ois.readObject();
-	    
-	    // Print rowNum and matrix
-	    System.out.println("Row num: " + rowNum);
-	    System.out.println();
-	    matrixMax.print();
-	    
-	    int maxForRow = findMax(matrixMax.getRow(rowNum));
-	    
-	    // Print max
-	    System.out.println("Max: " + maxForRow);
-	    
-	    oos.writeObject(Integer.toString(maxForRow));
-	    
-	    // Close Streams
-	    ois.close();
-	    oos.close();
-	    
-	    // Close Socket
-	    socket.close();
+	    System.out.println("Please enter the MatrixMaxServer Address: ");
+		String matrixMaxServerAddress = keyboard.readLine();
+		
+		System.out.println("Please enter the MatrixMaxServer Port: ");
+		int matrixMaxServerPort = Integer.parseInt(keyboard.readLine());
+		
+		new MatrixMaxClientThread(matrixMaxServerAddress, matrixMaxServerPort).start();  
 	}
 }
