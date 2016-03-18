843f56931867aa7702f816af9378aca344c3ea43
Rootul Patel <rootulp@gmail.com>
2016-03-17 22:21:06 -0400

    hw4: retab and remove trailing whitespace

--- distributed_systems/hw4/src/hw4/client/Client.java
+++ distributed_systems/hw4/src/hw4/client/Client.java
@@ -1,8 +1,10 @@
 package hw4.client;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,75 +14,82 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-	
-	private static class ClientThread extends Thread {
-        
-        private String middlewareAddress;
-        private int middlewarePort;
-        private String filename;
-        private String transferType;
-        private Socket middlewareSocket;
-        private Socket serverSocket;
-        
-        public ClientThread(String middlewareAddress, int middlewarePort, String filename, String transferType) {
-            this.middlewareAddress = middlewareAddress;
-            this.middlewarePort = middlewarePort;
-            this.filename = filename;
-            this.transferType = transferType;
-        }
 
-        public void run() {
-            try {
-            	// Middleware Setup
-            	middlewareSocket = new Socket(middlewareAddress, middlewarePort);
-        		BufferedReader middlewareIn = new BufferedReader(new InputStreamReader(middlewareSocket.getInputStream()));
-        		PrintWriter middlewareOut = new PrintWriter(middlewareSocket.getOutputStream(), true);
-        	    
-        		// Write filename to Middleware
-        	    middlewareOut.println(filename);
-        	    
-        	    // Get Middleware response
-        		String serverAddress = middlewareIn.readLine();
-        		int serverPort = Integer.parseInt(middlewareIn.readLine());
-        		System.out.println("File " + filename + " is located at Server Address: " + serverAddress + " Port: " + serverPort);
-        		
-        		// Server Setup
-        		serverSocket = new Socket(serverAddress, serverPort);
-               
-            } catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-                try {
-                	middlewareSocket.close();
-                	serverSocket.close();
-                } catch (IOException e) {
-                	// TODO Auto-generated catch block
-    				e.printStackTrace();
-                }
-            }
-        }
+  private static class ClientThread extends Thread {
+
+    private String middlewareAddress;
+    private int middlewarePort;
+    private String filename;
+    private String transferType;
+    private Socket middlewareSocket;
+    private Socket serverSocket;
+
+    public ClientThread(String middlewareAddress, int middlewarePort, String filename, String transferType) {
+      this.middlewareAddress = middlewareAddress;
+      this.middlewarePort = middlewarePort;
+      this.filename = filename;
+      this.transferType = transferType;
     }
-	
-	public static void main(String argv[]) throws IOException {
 
-	    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-	    
-	    System.out.println("Please enter the Middleware Address: ");
-		String middlewareAddress = keyboard.readLine();
-		
-		System.out.println("Please enter the Middleware Port: ");
-		int middlewarePort = Integer.parseInt(keyboard.readLine());
-			    
-		while (true) {
-			System.out.println("Please enter the filename: ");
-			String filename = keyboard.readLine();
-			System.out.println("Please enter 'download' or 'upload': ");
-			String transferType = keyboard.readLine();
-            new ClientThread(middlewareAddress, middlewarePort, filename, transferType).start();
+    public void run() {
+      try {
+        // Middleware Setup
+        middlewareSocket = new Socket(middlewareAddress, middlewarePort);
+        BufferedReader middlewareIn = new BufferedReader(new InputStreamReader(middlewareSocket.getInputStream()));
+        PrintWriter middlewareOut = new PrintWriter(middlewareSocket.getOutputStream(), true);
+
+        // Write filename to Middleware
+        middlewareOut.println(filename);
+
+        // Get Middleware response
+        String serverAddress = middlewareIn.readLine();
+        int serverPort = Integer.parseInt(middlewareIn.readLine());
+        System.out.println("File " + filename + " is located at Server Address: " + serverAddress + " Port: " + serverPort);
+
+        // Server Setup
+        serverSocket = new Socket(serverAddress, serverPort);
+
+        byte[] mybytearray = new byte[1024];
+        InputStream is = serverSocket.getInputStream();
+        FileOutputStream fos = new FileOutputStream(filename);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+        bos.write(mybytearray, 0, bytesRead);
+
+      } catch (UnknownHostException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } finally {
+        try {
+          middlewareSocket.close();
+          serverSocket.close();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
         }
-	}
+      }
+    }
+  }
+
+  public static void main(String argv[]) throws IOException {
+
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Please enter the Middleware Address: ");
+    String middlewareAddress = keyboard.readLine();
+
+    System.out.println("Please enter the Middleware Port: ");
+    int middlewarePort = Integer.parseInt(keyboard.readLine());
+
+    while (true) {
+      System.out.println("Please enter the filename: ");
+      String filename = keyboard.readLine();
+      System.out.println("Please enter 'download' or 'upload': ");
+      String transferType = keyboard.readLine();
+      new ClientThread(middlewareAddress, middlewarePort, filename, transferType).start();
+    }
+  }
 }
--- distributed_systems/hw4/src/hw4/middleware/Middleware.java
+++ distributed_systems/hw4/src/hw4/middleware/Middleware.java
@@ -10,93 +10,93 @@ import java.net.UnknownHostException;
 import java.net.InetAddress;
 
 public class Middleware {
-	
-	private static class MiddlewareThread extends Thread {
-        
-		private Socket socket;
-		private String[] serverAddresses;
-		private int[] serverPorts;
-        
-        public MiddlewareThread(Socket socket, String[] serverAddresses, int[] serverPorts) {
-            this.socket = socket;
-            this.serverAddresses = serverAddresses;
-            this.serverPorts = serverPorts;
-        }
 
-        public void run() {
-            try {        		
-        		// Stream Setup
-        		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        	            	   
-        		String filename = in.readLine();
-        		int serverNumber = hashFilename(filename);
-    			String serverAddress = serverAddresses[serverNumber];
-    			int serverPort = serverPorts[serverNumber];
-    			out.println(serverAddress);
-    			out.println(serverPort);
-                System.out.println("Routed request for the File " + filename + " to server number: " + serverNumber + " at Address: " + serverAddress + " Port: " + serverPort);
-            } catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                	// TODO Auto-generated catch block
-    				e.printStackTrace();
-                }
-            }
-        }
+  private static class MiddlewareThread extends Thread {
+
+    private Socket socket;
+    private String[] serverAddresses;
+    private int[] serverPorts;
+
+    public MiddlewareThread(Socket socket, String[] serverAddresses, int[] serverPorts) {
+      this.socket = socket;
+      this.serverAddresses = serverAddresses;
+      this.serverPorts = serverPorts;
     }
 
-	// Returns 0, 1, or 2 depending on which server has the file
-	private static int hashFilename(String filename) {
-	    int total = 0;
-		for (char ch : filename.toCharArray()){
-			// Convert char to ASCII value then subtract 96
-			// so a is 1, b is 2, etc.
-			total += (int) ch - 96;
-	    }
-		return total % 3;
-	}
+    public void run() {
+      try {
+        // Stream Setup
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-	// Pass port number as first argument
-	public static void main(String[] args) throws Exception {
-
-	    // Port and Socket Setup
-	    int portNumber = Integer.parseInt(args[0]);
-	    ServerSocket middlewareSocket = new ServerSocket(portNumber);
-	    
-	    // Initial Server Setup
-	    // Sketchy that I'm storing this info in two separate arrays
-	    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-	    String[] serverAddresses = new String[3];
-	    int[] serverPorts = new int[3];
-	    
-	    for (int serverNum = 0; serverNum <= 2; serverNum++) {
-	    	System.out.println("Please enter the Address for Server: " + serverNum);
-			serverAddresses[serverNum] = keyboard.readLine();
-			
-			System.out.println("Please enter the Port for Server: " + serverNum);
-			serverPorts[serverNum] = Integer.parseInt(keyboard.readLine());
-	    }
-	    
-	    // Print Address and Port
-	    System.out.println("Middleware waiting for clients to connect on:");
-	    System.out.println("Address: " + InetAddress.getLocalHost().getHostAddress());
-	    System.out.println("Port: " + middlewareSocket.getLocalPort());
-	    System.out.println();
-
-	    try {
-            while (true) {
-                new MiddlewareThread(middlewareSocket.accept(), serverAddresses, serverPorts).start();
-            }
-        } finally {
-            middlewareSocket.close();
+        String filename = in.readLine();
+        int serverNumber = hashFilename(filename);
+        String serverAddress = serverAddresses[serverNumber];
+        int serverPort = serverPorts[serverNumber];
+        out.println(serverAddress);
+        out.println(serverPort);
+        System.out.println("Routed request for the File " + filename + " to server number: " + serverNumber + " at Address: " + serverAddress + " Port: " + serverPort);
+      } catch (UnknownHostException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } finally {
+        try {
+          socket.close();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
         }
-	}
+      }
+    }
+  }
+
+  // Returns 0, 1, or 2 depending on which server has the file
+  private static int hashFilename(String filename) {
+    int total = 0;
+    for (char ch : filename.toCharArray()){
+      // Convert char to ASCII value then subtract 96
+      // so a is 1, b is 2, etc.
+      total += (int) ch - 96;
+    }
+    return total % 3;
+  }
+
+  // Pass port number as first argument
+  public static void main(String[] args) throws Exception {
+
+    // Port and Socket Setup
+    int portNumber = Integer.parseInt(args[0]);
+    ServerSocket middlewareSocket = new ServerSocket(portNumber);
+
+    // Initial Server Setup
+    // Sketchy that I'm storing this info in two separate arrays
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    String[] serverAddresses = new String[3];
+    int[] serverPorts = new int[3];
+
+    for (int serverNum = 0; serverNum <= 2; serverNum++) {
+      System.out.println("Please enter the Address for Server: " + serverNum);
+      serverAddresses[serverNum] = keyboard.readLine();
+
+      System.out.println("Please enter the Port for Server: " + serverNum);
+      serverPorts[serverNum] = Integer.parseInt(keyboard.readLine());
+    }
+
+    // Print Address and Port
+    System.out.println("Middleware waiting for clients to connect on:");
+    System.out.println("Address: " + InetAddress.getLocalHost().getHostAddress());
+    System.out.println("Port: " + middlewareSocket.getLocalPort());
+    System.out.println();
+
+    try {
+      while (true) {
+          new MiddlewareThread(middlewareSocket.accept(), serverAddresses, serverPorts).start();
+      }
+    } finally {
+      middlewareSocket.close();
+    }
+  }
 }
